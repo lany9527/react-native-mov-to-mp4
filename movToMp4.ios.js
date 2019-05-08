1c6aec60e3170ea73c58375a7a3d5c5d24c11295
LY9527 <lany9527@gmail.com>
2019-05-08 15:49:05 +0800

    Update movToMp4.ios.js

--- movToMp4.ios.js
+++ movToMp4.ios.js
@@ -2,27 +2,32 @@
  * @providesModule movToMp4
  * @flow
  */
-import React, { Component, PropTypes } from 'react';
 import {
-    DeviceEventEmitter, // android
-    NativeAppEventEmitter, // ios
-    NativeModules,
-    Platform,
-    StyleSheet,
-    requireNativeComponent,
-    View,
+  Platform,
+  NativeModules,
 } from 'react-native';
 
+var warning = require('fbjs/lib/warning');
+
+if (Platform.OS === 'ios') {
 //var NativemovToMp4 = require('NativeModules').movToMp4;
-var NativemovToMp4 = NativeModules.movToMp4;
-/**
- * High-level docs for the movToMp4 iOS API can be written here.
- */
+  var NativemovToMp4 = NativeModules.movToMp4;
+  /**
+   * High-level docs for the movToMp4 iOS API can be written here.
+   */
 
-var movToMp4 = {
-  convertMovToMp4: function(filename, dest, cb) {
-    NativemovToMp4.convertMovToMp4(filename, dest, cb);
-  }
-};
+  var movToMp4 = {
+    convertMovToMp4: NativemovToMp4.convertMovToMp4,
+  };
 
-module.exports = movToMp4;
+  module.exports = movToMp4;
+} else {
+
+  var movToMp4 = {
+    test: function() {
+      warning('Not yet implemented for Android.');
+    }
+  };
+
+  module.exports = movToMp4;
+}
